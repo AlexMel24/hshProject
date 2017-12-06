a7d32f4a01cf77f48c05389f07ea2b310d4f4e37
Alex <frunzik92@gmail.com>
2017-12-06 16:09:39 +0300

    adding js file

--- app/controllers/java_script_hash_controller.rb
+++ app/controllers/java_script_hash_controller.rb
@@ -1,5 +1,17 @@
 class JavaScriptHashController < ApplicationController
+  before_action :is_user_loged?
+
   def jsshoh
 
   end
+
+  private
+
+  def is_user_loged?
+    if session[:user_id].nil?
+      flash.now[:success] = "Log in, first, please!"
+      redirect_to login_path
+    end
+  end
+
 end
