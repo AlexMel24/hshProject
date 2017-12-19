018a9c79871d010cfce61d794fc719fe5a27539d
Alex <frunzik92@gmail.com>
2017-12-19 18:13:07 +0300

    adding js file

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
+
+  has_many :user_activity
+
   before_save { self.email = email.downcase }
   validates(:name, presence: true, length: { maximum: 50 })
   validates(:password, presence: true, length: { minimum: 6 })
